92ba2bb87024646df292992385dd2a89b94be4e9
heyuall <zoubeidihoussem@gmail.com>
2024-03-05 21:46:37 +0100

    Create project

---
+++ src/main/java/com/notif_back_end/notifbackend/NotifBackendApplication.java
@@ -0,0 +1,14 @@
+package com.notif_back_end.notifbackend;
+
+import com.azure.spring.messaging.implementation.annotation.EnableAzureMessaging;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotifBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/notif_back_end/notifbackend/NotifBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notif_back_end.notifbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
